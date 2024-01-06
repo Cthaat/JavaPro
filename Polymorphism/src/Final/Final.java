3ae098b37ae1b1d48654f1e6135c03a8c8398296
kc++ <2745296803@qq.com>
2024-01-06 12:48:12 +0800

    final

---
+++ Polymorphism/src/Final/Final.java
@@ -0,0 +1,10 @@
+package Final;
+
+public class Final
+{
+    public static final String AUTHOR = "John Doe";
+    public static void main(String[] args)
+    {
+        System.out.println(AUTHOR);
+    }
+}
