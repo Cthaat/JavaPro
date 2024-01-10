2c7a5178a482bb7d45183cdd4d85ee3baa1d6fe6
kc++ <2745296803@qq.com>
2024-01-10 15:29:47 +0800

    API

---
+++ API/src/date/arr.java
@@ -0,0 +1,12 @@
+package date;
+
+import java.util.Arrays;
+
+public class arr
+{
+    public static void main(String[] args)
+    {
+        int[] arr = new int[]{1, 2, 3, 4, 5};
+        System.out.println(Arrays.toString(arr));
+    }
+}
