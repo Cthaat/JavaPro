3aad5288086801b5631bd5b0ddbc6396bb40e01e
kc++ <2745296803@qq.com>
2024-01-10 14:51:16 +0800

    API

--- API/src/date/per.java
+++ API/src/date/per.java
@@ -1,5 +1,15 @@
 package date;
 
+import java.time.LocalDate;
+import java.time.Period;
+
 public class per
 {
+    public static void main(String[] args)
+    {
+        Period a = Period.between(LocalDate.of(2000 , 1 , 1) , LocalDate.now());
+        System.out.println(a.getDays());
+        System.out.println(a.getMonths());
+        System.out.println(a.getYears());
+    }
 }
