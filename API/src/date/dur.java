ea5e56eca385791c09169257cde0f682db3c2383
kc++ <2745296803@qq.com>
2024-01-10 15:24:38 +0800

    API

--- API/src/date/dur.java
+++ API/src/date/dur.java
@@ -1,5 +1,16 @@
 package date;
 
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public class dur
 {
+    public static void main(String[] args)
+    {
+        Duration a = Duration.between(LocalDateTime.of(2023, 1, 1 , 11 , 10 , 10), LocalDateTime.of(2023, 1, 10 , 11 , 10 , 15));
+        System.out.println(a.toDays());
+        System.out.println(a.toHours());
+        System.out.println(a.toNanos());
+    }
 }
