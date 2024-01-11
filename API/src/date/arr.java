9673c1cff06b8bb69b6bb6ac260e47cca6562c8c
kc++ <2745296803@qq.com>
2024-01-11 10:40:09 +0800

    API

--- API/src/date/arr.java
+++ API/src/date/arr.java
@@ -1,6 +1,8 @@
 package date;
 
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.function.IntToDoubleFunction;
 
 public class arr
 {
@@ -8,5 +10,31 @@ public class arr
     {
         int[] arr = new int[]{1, 2, 3, 4, 5};
         System.out.println(Arrays.toString(arr));
+        int[] arr2 = Arrays.copyOfRange(arr, 1, 4);
+        System.out.println(Arrays.toString(arr2));
+        int[] arr3 = Arrays.copyOf(arr, 30);
+        System.out.println(Arrays.toString(arr3));
+        double[] arr4 = new double[]{1.5, 2.5, 3.5};
+        Arrays.setAll(arr4 , new IntToDoubleFunction()
+        {
+            @Override
+            public double applyAsDouble(int value)
+            {
+                return value * 2;
+            }
+        });
+        System.out.println(Arrays.toString(arr4));
+        Integer[] arr5 = new Integer[]{9,1,6,51,9,12,6,16,1,6};
+        Arrays.sort(arr5);
+        System.out.println(Arrays.toString(arr5));
+        Arrays.sort(arr5 , new Comparator<Integer>()
+        {
+            @Override
+            public int compare(Integer o1, Integer o2)
+            {
+                return o2 - o1;
+            }
+        });
+        System.out.println(Arrays.toString(arr5));
     }
 }
