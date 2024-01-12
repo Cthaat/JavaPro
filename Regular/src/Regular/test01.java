799f469c27d667c7f4450f4158966310f163664c
kc++ <2745296803@qq.com>
2024-01-12 11:40:32 +0800

    Regular

---
+++ Regular/src/Regular/test01.java
@@ -0,0 +1,29 @@
+package Regular;
+
+public class test01
+{
+    public static void main(String[] args)
+    {
+        System.out.println("a".matches("[abc]"));
+        System.out.println("e".matches("[abcd]"));
+        String a;
+        a = "16630979651";
+        System.out.println(a.matches("(1[3-9]\\d{9})|(0\\d{2}-?\\d{8})"));
+    }
+
+    public static boolean checkQQ(String QQ)
+    {
+        if (QQ == null || QQ.startsWith("0") || QQ.length() > 11 || QQ.length() < 5)
+        {
+            return false;
+        }
+        for (int i = 0 ; i < QQ.length() ; i++)
+        {
+            if (QQ.charAt(i) < '0' || QQ.charAt(i) > '9')
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
