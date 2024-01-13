192d5ff54583b2afda680abda3292ed84a98f25b
kc++ <2745296803@qq.com>
2024-01-13 10:49:34 +0800

    Regular

--- Regular/src/Regular/test02.java
+++ Regular/src/Regular/test02.java
@@ -1,5 +1,16 @@
 package Regular;
 
+import java.util.regex.Pattern;
+
 public class test02
 {
+    public static void main(String[] args)
+    {
+
+    }
+    public static void method1()
+    {
+        String regu = "(1[3-9]\\d{9})|(0\\d{2,7}-?\\d{7})|(\\w{2,}@\\w{2,20}\\.\\w{2,6})";
+        Pattern pattern = Pattern.compile(regu);
+    }
 }
